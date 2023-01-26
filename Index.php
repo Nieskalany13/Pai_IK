0eee0c58184556bca39d149d3d7d0cf293d04ecf
Nieskalany13 <100197967+Nieskalany13@users.noreply.github.com>
2023-01-26 12:43:28 +0100

    lekcja1

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php
+//Zad1 [A]
+/* $x = 10;
+if ($x >= 0) 
+{
+echo ("Wartosć bezwględna wynosi".$x);}
+else {
+echo ("Wartość bezwględna wynosi".$x);
+}
+*/
+
+//[B]
+/*
+$x = -10 ;
+echo $x>=0?$x:$x;
+*/
+
+?>
