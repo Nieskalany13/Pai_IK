5654134742566271f722faca7dc30c37bea6b67d
Nieskalany13 <100197967+Nieskalany13@users.noreply.github.com>
2023-01-26 13:17:22 +0100

    Lekcja1-2

---
+++ Index_pętle.php
@@ -0,0 +1,46 @@
+<?php
+//Zad_1
+/*
+            for($count = 5; $count <= 15; ++$count)
+            {
+                echo $count ."<br>";
+            }
+*/
+
+//Zad_2
+/*
+            $a = 1;
+            $b = 10;
+            for($count = $a; $count <= $b; ++$count)
+            {
+                echo $count ."<br>";
+            }
+*/
+
+//Zad_3
+/*
+            $Imie = "Mateusz";
+            for($count = 1; $count <= 5; ++$count)
+            {
+                echo $Imie."<br>";
+            }
+*/
+
+//Zad_4
+/*
+            $Imie = "Mateusz";
+            for($Imie = 1; $Imie <= 10; ++$Imie)
+            {
+                echo $Imie."Mateusz"."<br>";
+            }
+*/
+
+//Zad_5
+/*
+            $znak ="**********";
+            for($count = 1; $count <= 5; ++$count)
+            {
+                echo $znak."<br>";
+            }
+*/
+?>
