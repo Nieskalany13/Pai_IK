fee1f2f50f1f49140265d92fc9a2ae95c328f9f3
Nieskalany13 <100197967+Nieskalany13@users.noreply.github.com>
2023-01-30 15:02:45 +0100

    pętla while

---
+++ Index 3.php
@@ -0,0 +1,104 @@
+<?php
+    /*$count = 1;
+    do {
+        $count++;
+        Echo "$count pomnożone prze 10 wynosi ".$count * 12;
+        echo "<br>";
+    }
+    while ($count <= 5);
+    */
+
+    //Zad 1
+
+    //pętla while
+        /*$liczba = 5;
+        while ($liczba <= 15) {
+            echo ($liczba);
+            echo '<br>';
+            $liczba++;
+        }
+        
+*/
+        //pętla do while
+    $count = 4;
+    do {
+        $count++;
+        Echo $count;
+        echo "<br>";
+    }
+    while ($count < 15);
+        echo "<br>";
+
+    //Zad 2
+
+    //pętla while
+    /*$a = 5;
+    $b = 15;
+    while ($a <= $b) {
+        $a++;
+        echo ($a);
+        echo '<br>';
+    }
+*/
+//pętla do while
+$a = 4;
+$b = 15;
+do {
+    $a++;
+    Echo $a;
+    echo "<br>";
+}
+while ($a < $b);
+    echo "<br>";
+
+    //Zad 3
+
+    //pętla while
+    /*$imie = 5;
+    while ($imie < 10) {
+        $imie++;
+        echo ($imie."Mateusz");
+        echo '<br>';
+    }
+*/
+    echo "<br>";
+
+    //Zad 4
+
+    //pętla while
+    /*$imie = 5;
+    while ($imie < 10) {
+        $imie++;
+        echo ("Mateusz");
+        echo '<br>';
+    }
+*/
+    echo "<br>";
+
+    //Zad 5
+
+    //pętla while
+    /*$prostokąt = 5;
+    while ($prostokąt < 10) {
+        $prostokąt++;
+        echo ("**********");
+        echo '<br>';
+    }
+*/
+    echo "<br>";
+
+    //Zad 6
+
+    //pętla while
+    /*$reszta = 50;
+    while ($reszta <= 200) {
+        if ($reszta % 11==0)
+        {
+            echo $reszta;
+        }
+        $reszta++;
+        echo '<br>';
+    }
+*/
+
+?>
